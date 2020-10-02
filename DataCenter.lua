2a786ed75f55867306dba4075a800f6be42b4b96
proton-c <56282105+proton-c@users.noreply.github.com>
2020-10-02 07:45:20 -0400

    Added Data Center Comment

--- DataCenter.lua
+++ DataCenter.lua
@@ -1,3 +1,4 @@
+-- This file would be placed in a server that would be used as a main data center server. It would be in the file location "/autorun.lua"
 local component = require("component")
 local event = require("event")
 local serial = require("serialization")
