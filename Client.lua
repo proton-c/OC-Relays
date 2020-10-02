8e546aa3873dd82ba141cdff2aca59b111d049d9
proton-c <56282105+proton-c@users.noreply.github.com>
2020-10-02 08:10:04 -0400

    Automatic Handshaking
    Before I added this, you'd have to manually execute a handshake program that wasn't included in this repo.
    
    Automatic handshaking is a lot easier.

--- Client.lua
+++ Client.lua
@@ -5,12 +5,41 @@ local serial = require("serialization")
 local m = component.modem
 local fs = require("filesystem")
 
+-- Port that we're going to send and receive data with
+local port = 80
+
+-- Port for handshakes
+local handshakePort = 90
+
+local function handshake()
+    print("Please enter a random number for the handshake:")
+    local num = io.read()
+    if type(num) ~= "number" then num = 60 end
+    local calc = (num + 50) * 2
+    m.open(handshakePort)
+    m.broadcast(handshakePort, num)
+    local _, _, from, inPort, _, msg = event.pull(5, "modem_message")
+    if msg == calc then
+        local file = fs.open("/home/data/address", "w")
+        file:write(from)
+        file:close()
+    elseif msg == nil then
+        print("Handshake failed! Timeout.")
+    else
+        print("Handshake failed! Relay returned incorrect calculation.")
+    end
+end
+
+if fs.exists("/home/data") == false then
+    fs.makeDirectory("/home/data")
+    handshake()
+elseif fs.exists("/home/data/address") == false then
+    handshake()
+end
+
 local file = fs.open("/home/data/address", "r")
 local address = file:read(500)
 file:close()
-
--- Port that we're going to send and receive data with
-local port = 80
 
 m.open(port)
 
